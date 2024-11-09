a6df11380f067baf8202a7086d70e395eb6cf403
Davit-AI <davit.khanal@8squarei.com>
2024-11-09 19:30:58 +0545

    Day 1 printing & input

---
+++ Day 1/main.py
@@ -0,0 +1,10 @@
+## Printing first line
+print("Welcome to the Band Name Generator.")
+
+## ask user for the input of city they grew and their favourite things
+cityName = input("What's name of the city you grew up in? \n")
+petName = input("What is something you like most? \n")
+
+## combine both to give unique names
+bandName = "Your bandname can be" + cityName + " " + petName
+print(bandName)
